8acce15d33996faf8d262375f13ebff19570cce0
wmorgen1 <97263265+wmorgen1@users.noreply.github.com>
2022-11-16 13:41:33 -0500

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,40 @@
+#' Example VirScan hits fold change antibody profile
+#'
+#' Data generated from one serum sample by the Larman Lab
+#'
+#' @format ## `example_virscan_hfc`
+#' A data frame
+#' 
+#' @source Larman Lab
+"example_virscan_hfc"
+
+#' Example ToxScan hits fold change antibody profile
+#'
+#' Data generated from one serum sample by the Larman Lab
+#'
+#' @format ## `example_toxscan_hfc`
+#' A data frame
+#' 
+#' @source Larman Lab
+"example_toxscan_hfc"
+
+#' Example VirScan fold change antibody profile
+#'
+#' Data generated from one serum sample by the Larman Lab
+#'
+#' @format ## `example_virscan_fc`
+#' A data frame
+#' 
+#' @source Larman Lab
+"example_virscan_fc"
+
+#' Example ToxScan fold change antibody profile
+#'
+#' Data generated from one serum sample by the Larman Lab
+#'
+#' @format ## `example_virscan_fc`
+#' A data frame
+#' 
+#' @source Larman Lab
+"example_toxscan_fc"
+
